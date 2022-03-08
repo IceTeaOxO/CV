00ba67430e3c38959bae802544860989c710f6d3
IceTeaOxO <86127014+IceTeaOxO@users.noreply.github.com>
2022-03-08 17:17:15 +0800

    hw1-final

--- hw1.py
+++ hw1.py
@@ -4,8 +4,8 @@ import cv2
 import numpy as np
 import random
 
-
-def filter(img,kernel):
+#'''
+def filter(img,kernel,name):
     img_result = img
     image = np.pad(img,1,mode='constant')
 
@@ -26,37 +26,51 @@ def filter(img,kernel):
 
     #print(image.shape)
     cv2.imshow('result',img_result)
+    cv2.imwrite("result"+name+".jpg",img_result)
+
     cv2.imshow("origin",image)
+    cv2.imwrite("origin.jpg",image)
     print(img.shape)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-
+#'''
 if(__name__=='__main__'):
     img = cv2.imread("../computer_vision/0.jpg")
     img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     #src_img = cv2.imread("../computer_vision/0.jpg")
-    #'''
-    kernel1 = np.array([
+    '''
+    kernel = np.array([
         [0,-1,0],
         [-1,5,-1],
         [0,-1,0]
     ])
     #print(type(img))
-    filter(img,kernel1)
-    #'''
+       
+
+    filter(img,kernel,"1")
+
+    '''
     '''
     kernel2 = np.array([
         [1,0,-1],
         [1,0,-1],
         [1,0,-1]
     ])
-    filter(img,kernel2)
+    filter(img,kernel2,"2")
     '''
-    '''
+
+    #'''
     kernel3 = np.array([
         [-1,-1,-1],
         [0,0,0],
         [1,1,1]
     ])
-    filter(img,kernel3)
-    '''
+    filter(img,kernel3,"3")
+    #'''
+
+    image2 = img
+    filter2d_img = cv2.filter2D(image2,-1,kernel3)
+    cv2.imshow("filter2D img",filter2d_img)
+    cv2.imwrite("Filter2d Sharpened Image3.jpg",filter2d_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows() 
